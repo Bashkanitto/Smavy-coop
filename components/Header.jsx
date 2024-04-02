79e2050639f1c73737c3fc2d8e3ec182ccfaea83
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-02 16:16:48 +0600

    fix header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,69 +1,66 @@
-'use client';
-import React, { useState } from 'react';
-import { ArrowUpRight } from 'lucide-react';
-import { Menu } from 'lucide-react';
-import Link from 'next/link';
+"use client";
+import React, { useState } from "react";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { CustomButton } from "./CustomButton";
 
 const Header = () => {
-	const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-	const toggleMenu = () => {
-		setIsOpen(!isOpen);
-	};
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
 
-	return (
-		<header className='container mx-auto p-0 sm:py-4'>
-			{/* phone */}
-			<nav
-				style={{
-					transition: '0.5s',
-					height: isOpen ? '200px' : '0',
-				}}
-				className='absolute bg-slate-400 w-full flex justify-between items-center sm:hidden'
-			>
-				<ul
-					style={{
-						display: 'flex',
-						fontSize: isOpen ? '12px' : '0',
-					}}
-					className='hidden sm:flex flex-col gap-4 text-black text-xs'
-				>
-					<Link href='#About'>О нас</Link>
-					<Link href='#'>Услуги</Link>
-					<Link href='#'>Портфолио</Link>
-					<Link href='#'>FAQ</Link>
-					<Link href='#'>Контакты</Link>
-					<Link></Link>
-					Оставить заявку
-				</ul>
-			</nav>
-			<button
-				onClick={toggleMenu}
-				className='flex sm:hidden text-black focus:outline-none'
-			>
-				<Menu className='absolute right-6' />
-			</button>
+  return (
+    <header className="container mx-auto p-0 sm:py-4 min-h-10">
+      {/* phone */}
+      <nav
+        style={{
+          transition: "0.5s",
+          height: isOpen ? "200px" : "0",
+        }}
+        className="absolute bg-slate-400 w-full flex justify-between items-center sm:hidden z-20"
+      >
+        <ul
+          style={{
+            display: "flex",
+            fontSize: isOpen ? "12px" : "0",
+          }}
+          className="hidden sm:flex flex-col gap-4 text-black text-xs"
+          onClick={toggleMenu}
+        >
+          <Link href="#">О нас</Link>
+          <Link href="#">Услуги</Link>
+          <Link href="#">Портфолио</Link>
+          <Link href="#">FAQ</Link>
+          <Link href="#">Контакты</Link>
+          <Link href="#">Оставить заявку</Link>
+        </ul>
+      </nav>
+      <button
+        onClick={toggleMenu}
+        className="absolute right-2 top-2 flex sm:hidden text-black focus:outline-none z-50"
+      >
+        <Menu className="" />
+      </button>
 
-			{/* desktop */}
-			<nav className='sm:flex hidden justify-between items-center border rounded-[20px] border-gray-200 bg-gray-200 h-12 p-4'>
-				<div className='flex gap-5 md:gap-20 items-center'>
-					<img src='smavy2.png' className='top-1 h-4 md:h-7' />
-					<ul className='flex gap-4 text-black text-xs'>
-						<Link href='#About'>О нас</Link>
-						<Link href='#'>Услуги</Link>
-						<Link href='#'>Портфолио</Link>
-						<Link href='#'>FAQ</Link>
-						<Link href='#'>Контакты</Link>
-					</ul>
-				</div>
+      {/* desktop */}
+      <nav className="sm:flex hidden justify-between items-center border rounded-[20px] border-gray-200 bg-gray-200 h-12 px-4 py-2">
+        <div className="flex gap-5 md:gap-20 items-center">
+          <img src="smavy2.png" className="top-1 h-4 md:h-7" />
+          <ul className="flex gap-4 text-black text-xs">
+            <Link href="#About">О нас</Link>
+            <Link href="#">Услуги</Link>
+            <Link href="#">Портфолио</Link>
+            <Link href="#">FAQ</Link>
+            <Link href="#">Контакты</Link>
+          </ul>
+        </div>
 
-				<button className='bg-primary text-white text-xs rounded-full w-32 h-8 flex items-center justify-center'>
-					Оставить заявку
-					<ArrowUpRight className='w-4 h-4 ml-1' />
-				</button>
-			</nav>
-		</header>
-	);
+        <CustomButton>Оставить заявку</CustomButton>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
