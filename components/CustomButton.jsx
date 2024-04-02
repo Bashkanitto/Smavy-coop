8135706454e6ac954c2eea3786df653855d4483e
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-02 15:28:16 +0600

    fix customButton

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import { ArrowUpRight } from "lucide-react";
 
 export const CustomButton = ({ children }) => {
-	return (
-		<button className='bg-primary text-white rounded-3xl w-40 h-[50px] ml-1'>
-			{children}
-		</button>
-	);
+  return (
+    <button
+      className={`bg-primary text-white text-xs rounded-3xl w-40 h-10 ml-1 flex items-center justify-center`}
+    >
+      {children}
+      <ArrowUpRight className="w-4" />
+    </button>
+  );
 };
