5fd1bd8ca8547c04f8e24fd1d016da1e5299a56b
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-02 15:10:31 +0600

    clear trash

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-	],
-	theme: {
-		extend: {
-			colors: {
-				primary: '#4B40BB',
-				secondary: '#343434',
-			},
-		},
-	},
-	plugins: [],
+  content: [
+    "./pages/**/*.{js,jsx,}",
+    "./components/**/*.{js,jsx,}",
+    "./app/**/*.{js,jsx,}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4B40BB",
+        secondary: "#343434",
+      },
+    },
+  },
+  plugins: [],
 };
