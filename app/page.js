603ac5d4af94e9bda26ded19b64e7ac9131fd8ed
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-02 15:27:14 +0600

    add components to page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
-import PortfolioSection from '@/components/PortfolioSection';
+import Contact from "/components/Contact";
+import Footer from "/components/Footer";
+import Hero from "/components/Hero";
+import Benefits from "/components/Benefits";
+import Services from "/components/Services";
+import Faq from "/components/Faq";
+import PortfolioSection from "/components/PortfolioSection";
+import Header from "/components/Header";
+import Share from "../components/Share";
 
 export default function App() {
-	return (
-		<>
-			<PortfolioSection />
-		</>
-	);
+  return (
+    <div className="flex flex-col gap-20 transition-all duration-500">
+      <Header />
+      <Hero />
+      <Benefits />
+      <Services />
+      <Share />
+      <PortfolioSection />
+      <Faq />
+      <hr />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
