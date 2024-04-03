25e0e40add89bef5e2eaaa8fc681d68d46bab255
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-03 16:25:53 +0600

    fix faq

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -3,8 +3,8 @@ import { Accordion } from "./Accordion";
 
 const Faq = () => {
   return (
-    <section className="container mx-auto p-2">
-      <h3 className="text-3xl text-center my-10">Faq</h3>
+    <section id="faq" className="container mx-auto p-2">
+      <h3 className="text-3xl text-center my-10">FAQ</h3>
       <Accordion list={faqList} />
     </section>
   );
