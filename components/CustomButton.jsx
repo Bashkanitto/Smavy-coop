f9503bb20957c53b0b72b435888b9ecfa8bba311
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-03 16:18:56 +0600

    fix  custombtn style

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -3,7 +3,7 @@ import { ArrowUpRight } from "lucide-react";
 export const CustomButton = ({ children }) => {
   return (
     <button
-      className={`bg-primary text-white text-xs rounded-3xl w-40 h-10 ml-1 flex items-center justify-center`}
+      className={`btn bg-primary text-white text-xs rounded-3xl w-40 h-10 ml-1 flex items-center justify-center`}
     >
       {children}
       <ArrowUpRight className="w-4" />
